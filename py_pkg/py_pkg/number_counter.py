c36c5c530cfd37c9a9bc3379f7f05f5fdc01f78f
Ahmad Didik Setiyadi <adidiksetiyadi@gmail.com>
2023-05-23 16:58:04 +0700

    add resetter number

--- py_pkg/py_pkg/number_counter.py
+++ py_pkg/py_pkg/number_counter.py
@@ -3,6 +3,7 @@ import rclpy
 from rclpy.node import Node
  
 from example_interfaces.msg import Int64
+from example_interfaces.srv import SetBool
  
 class NumberCounterNode(Node): # MODIFY NAME
     def __init__(self):
@@ -10,6 +11,7 @@ class NumberCounterNode(Node): # MODIFY NAME
         self.counter_ = 0
         self.number_counter_publisher = self.create_publisher(Int64, "number_count", 10)
         self.number_subscriber_ = self.create_subscription(Int64, "number", self.number_callback, 10)
+        self.reset_counnter_service_ = self.create_service(SetBool, "reset_counter", self.reset_counter_callback)
         self.get_logger().info("Number Counter has been started")
     
     def number_callback(self, msg):
@@ -18,6 +20,16 @@ class NumberCounterNode(Node): # MODIFY NAME
         new_msg = Int64()
         new_msg.data = self.counter_
         self.number_counter_publisher.publish(new_msg)
+    
+    def reset_counter_callback(self, request, response):
+        if request.data:
+            self.counter_ = 0
+            response.success = True
+            response.message = "Counter has been reset"
+        else:
+            response.success = False
+            response.message = "Counter has NOT been reset"
+        return response
  
  
 def main(args=None):
