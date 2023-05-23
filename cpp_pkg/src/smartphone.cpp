65618187b80ddf55a65827ac4a663f9e9367aa84
Ahmad Didik Setiyadi <adidiksetiyadi@gmail.com>
2023-05-23 10:30:44 +0700

    add cpp publisher subscriber

---
+++ cpp_pkg/src/smartphone.cpp
@@ -0,0 +1,33 @@
+#include <memory>
+#include "rclcpp/rclcpp.hpp"
+#include "example_interfaces/msg/string.hpp"
+
+using std::placeholders::_1;
+ 
+class SmartphoneNode : public rclcpp::Node // MODIFY NAME
+{
+public:
+    SmartphoneNode() : Node("smartphone") // MODIFY NAME
+    {
+      subscriber_ = this->create_subscription<example_interfaces::msg::String>(
+      "robot_news", 10, std::bind(&SmartphoneNode::callbackRobotNews, this, _1));
+      RCLCPP_INFO(this->get_logger(), "Smartphone has been started");
+    }
+ 
+private:
+  rclcpp::Subscription<example_interfaces::msg::String>::SharedPtr subscriber_;
+
+  void callbackRobotNews(const example_interfaces::msg::String::SharedPtr msg)
+  {
+    RCLCPP_INFO(this->get_logger(), "%s", msg->data.c_str());
+  }
+};
+ 
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<SmartphoneNode>(); // MODIFY NAME
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
